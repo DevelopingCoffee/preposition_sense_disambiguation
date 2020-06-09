a36184447790a0f30eeab9e5420893ce1ca91292
Tim-orius <tim.rosenkranz1@t-online.de>
2020-06-09 22:15:33 +0200

    Now looping through directory

--- Flair/flair_text_classification/Flair_prepare.py
+++ Flair/flair_text_classification/Flair_prepare.py
@@ -1,54 +1,60 @@
 import xml.etree.ElementTree as ET
 import csv
 import pandas as pd
+import os
 
-root = ET.parse('../../SemEval/Train/Source/pp-with.sents.trng.xml').getroot()
+rootdir = '../../SemEval/Train/Source/'
+
 allPrep = []
 
-for type_tag in root.findall('instance'):
-    element = []
-    answer = type_tag.find("answer")
+# Go thorugh all files
+for file in os.scandir(rootdir):
+    file = str(file)[11:-2]
+    print(file)
+    root = ET.parse(rootdir+str(file)).getroot()
 
-    instance_id = answer.get("instance")
-    element.append(instance_id)
+    # Go through the file
+    for type_tag in root.findall('instance'):
+        element = []
+        answer = type_tag.find("answer")
 
-    sense_id = answer.get("senseid")
-    #print(sense_id)
-    label_ids = ""
+        # Get sense_id (label)
+        sense_id = answer.get("senseid")
+        #print(sense_id)
+        label_ids = ""
 
-    multi = True
-    while(multi):
-        if(sense_id.__contains__(" ")):
-            id1, sense_id = sense_id.split(" ")
-            label_ids += "__label__"+id1+" "
-        else:
-            multi = False;
-            label_ids += "__label__"+sense_id+" "
-    label_ids = label_ids[0:-1]
-    #print(label_ids)
-    element.append(label_ids)
+        multi = True
+        while(multi):
+            if(sense_id.__contains__(" ")):
+                id1, sense_id = sense_id.split(" ", 1)
+                label_ids += "__label__"+id1+" "
+            else:
+                multi = False;
+                label_ids += "__label__"+sense_id+" "
+        label_ids = label_ids[0:-1]
+        #print(label_ids)
+        element.append(label_ids)
 
-    content = type_tag.find("context")
-    newString = ET.tostring(content, encoding="unicode")
-    newString = newString[9:-13].strip()
-    contentRaw = content.text
-    contentStriped = contentRaw.strip()
-    element.append(newString)
+        # Get sentence (text)
+        content = type_tag.find("context")
+        newString = ET.tostring(content, encoding="unicode")
+        newString = newString[9:-13].strip()
+        contentRaw = content.text
+        contentStriped = contentRaw.strip()
+        element.append(newString)
 
-    allPrep.append(element)
+        allPrep.append(element)
 
-rows = []
-for e in allPrep:
-    rows.append([e[1],e[2]])
-
+# Write all data into a single csv file
 with open('data/train_data.csv', 'wt') as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerows(rows)
+    writer.writerows(allPrep)
 
-data = pd.read_csv("./train_data.csv", encoding='latin-1').sample(frac=1).drop_duplicates()
+data = pd.read_csv("./data/train_data.csv", encoding='latin-1').sample(frac=1).drop_duplicates()
+
 #data = data[['v1', 'v2']].rename(columns={"v1": "label", "v2": "text"})
-
 #data['label'] = '__label__' + data['label'].astype(str)
-data.iloc[0:int(len(data) * 0.8)].to_csv('train.csv', sep='\t', index=False, header=False)
-data.iloc[int(len(data) * 0.8):int(len(data) * 0.9)].to_csv('test.csv', sep='\t', index=False, header=False)
-data.iloc[int(len(data) * 0.9):].to_csv('dev.csv', sep='\t', index=False, header=False);
+
+data.iloc[0:int(len(data) * 0.8)].to_csv('data/train.csv', sep='\t', index=False, header=False)
+data.iloc[int(len(data) * 0.8):int(len(data) * 0.9)].to_csv('data/test.csv', sep='\t', index=False, header=False)
+data.iloc[int(len(data) * 0.9):].to_csv('data/dev.csv', sep='\t', index=False, header=False);
--- Flair/flair_text_classification/predict.py
+++ Flair/flair_text_classification/predict.py
@@ -1,13 +1,10 @@
-from flair.data import Corpus
-from flair.datasets import TREC_6
-from flair.embeddings import WordEmbeddings, FlairEmbeddings, DocumentRNNEmbeddings
+from flair.data import Sentence
 from flair.models import TextClassifier
-from flair.trainers import ModelTrainer
 
 classifier = TextClassifier.load('resources/taggers/trec/final-model.pt')
 
 # create example sentence
-sentence = Sentence('I ran for three hours ?')
+sentence = Sentence('He went to the station.')
 
 # predict class and print
 classifier.predict(sentence)
