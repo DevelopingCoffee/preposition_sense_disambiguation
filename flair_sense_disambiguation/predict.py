ee794274ce11a2de2b607001fd6b7d6deead7319
Tim-orius <tim.rosenkranz1@t-online.de>
2020-07-06 16:26:24 +0200

    Flair: Added 2 more test predictions to predict.py

--- flair_sense_disambiguation/predict.py
+++ flair_sense_disambiguation/predict.py
@@ -5,14 +5,17 @@ import sys
 classifier = TextClassifier.load('resources/best-model.pt')
 
 # create example sentence
-# sentence = Sentence('Peter ambled <head>after</head> them and joined other fathers who would doubtless have to help with bootlaces .')
 if(len(sys.argv) < 2):
-    sentence = Sentence('Peter ambled <head>after</head> them and joined other fathers who would doubtless have to help with bootlaces .')
+    sentence = Sentence('Peter ambled <head>after</head> them and joined other fathers who would doubtless have to help with bootlaces .') # 5(2)
+    sentence2 = Sentence('The queen smiled and tweaked me gently <head>by</head> the cheek .') # 5(2)
+    sentence3 = Sentence('Gingerly I squeezed a bit <head>on</head> my fingertip .') # 3(1b)
 else:
     print(sys.argv[1])
     sentence = Sentence(sys.argv[1])
 
 # predict class and print
 classifier.predict(sentence)
+classifier.predict(sentence2)
+classifier.predict(sentence3)
 
-print(sentence.labels) # 5(2)
+print(sentence.labels +", "+ sentence2.labels +", "+ sentence3.labels)
