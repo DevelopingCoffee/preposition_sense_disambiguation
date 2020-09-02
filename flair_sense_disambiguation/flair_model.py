a047b7b06959eb5bcfb8b51d931b3457e11b2751
Tim-orius <tim.rosenkranz1@t-online.de>
2020-09-02 19:42:15 +0200

    Flair: Minor cleanup & Optimization optimization

--- flair_sense_disambiguation/flair_model.py
+++ flair_sense_disambiguation/flair_model.py
@@ -17,6 +17,10 @@ import csv
 import os
 from random import shuffle
 
+from hyperopt import hp
+from flair.hyperparameter.param_selection import SearchSpace, Parameter
+from flair.hyperparameter.param_selection import TextClassifierParamSelector, OptimizationValue
+
 
 class BaseModel:
     """Base Model for flair"""
@@ -69,20 +73,21 @@ class BaseModel:
         # Can choose between many RNN types (GRU by default, to change use rnn_type parameter)
         # document_embeddings = DocumentRNNEmbeddings(word_embeddings, hidden_size=256)
         #############################################################################
+
         # Instantiate one-hot encoded word embeddings with your corpus
-        # hot_embedding = OneHotEmbeddings(self.__corpus)
-        #
-        # # Init standard GloVe embedding
-        # glove_embedding = WordEmbeddings('glove')
-        #
-        # # Document pool embeddings
-        # document_embeddings = DocumentPoolEmbeddings([hot_embedding, glove_embedding], fine_tune_mode='none')
+        hot_embedding = OneHotEmbeddings(self.__corpus)
 
-        word_embeddings = [WordEmbeddings('glove'), FlairEmbeddings('news-forward-fast'),
-                           FlairEmbeddings('news-backward-fast')]
+        # Init standard GloVe embedding
+        glove_embedding = WordEmbeddings('glove')
 
-        document_embeddings = DocumentLSTMEmbeddings(word_embeddings, hidden_size=512, reproject_words=True,
-                                                     reproject_words_dimension=256)
+        # Document pool embeddings
+        document_embeddings = DocumentPoolEmbeddings([hot_embedding, glove_embedding], fine_tune_mode='none')
+
+        # word_embeddings = [WordEmbeddings('glove'), FlairEmbeddings('news-forward-fast'),
+        #                    FlairEmbeddings('news-backward-fast')]
+        #
+        # document_embeddings = DocumentLSTMEmbeddings(word_embeddings, hidden_size=512, reproject_words=True,
+        #                                              reproject_words_dimension=256)
 
         # Create the text classifier
         self.__classifier = TextClassifier(document_embeddings, label_dictionary=label_dict, multi_label=False)
@@ -101,7 +106,7 @@ class BaseModel:
         print(Corpus)
 
 
-    def train(self, data_dir="data/", mini_batch_size=32, learning_rate=0.1):
+    def train(self, data_dir="data/", mini_batch_size=32, learning_rate=0.1, epochs=10):
         """Train a model
            :param data dir: directory where training data is stored (optimal is train, test and dev file)
         """
@@ -127,114 +132,38 @@ class BaseModel:
                       mini_batch_size=mini_batch_size,
                       anneal_factor=0.5,
                       patience=5,
-                      max_epochs=10)
+                      max_epochs=epochs)
 
-    def predict(self, sentences):
-        """Predict a list of sentences
-           :param sentences: list of sentences to predict
-        """
+    def optimize(self):
 
-        if self.__classifier is None:
-            self._load_classifier()
-            if self.__classifier is None:
-                print("Prediction not possible.")
-                return
+        # define your search space
+        search_space = SearchSpace()
+        search_space.add(Parameter.EMBEDDINGS, hp.choice, options=[
+            [WordEmbeddings('en')],
+            [FlairEmbeddings('news-forward'), FlairEmbeddings('news-backward')],
+            [WordEmbeddings('glove')],
+            [WordEmbeddings('glove'), OneHotEmbeddings(self.__corpus)],
+            [FlairEmbeddings('news-forward'), FlairEmbeddings('news-backward'), OneHotEmbeddings(self.__corpus)]
+        ])
+        search_space.add(Parameter.HIDDEN_SIZE, hp.choice, options=[32, 64, 128])
+        search_space.add(Parameter.RNN_LAYERS, hp.choice, options=[1, 2])
+        search_space.add(Parameter.DROPOUT, hp.uniform, low=0.0, high=0.5)
+        search_space.add(Parameter.LEARNING_RATE, hp.choice, options=[0.05, 0.1, 0.15, 0.2])
+        search_space.add(Parameter.MINI_BATCH_SIZE, hp.choice, options=[8, 16, 32])
 
-        tagger = Tagger()
-        tagger.set_input(sentences)
-        dataset = tagger.do_tagging()
-
-        dataset = SentenceDataset([Sentence(text, SpaceTokenizer()) for text in dataset])
-        self.__classifier.predict(
-            dataset,
-            mini_batch_size=self.__mini_batch_size,
-            verbose=self.__verbose
+        # create the parameter selector
+        param_selector = TextClassifierParamSelector(
+            self.__corpus,
+            False,
+            'resources/optimization/results',
+            'lstm',
+            max_epochs=50,
+            training_runs=3,
+            optimization_value=OptimizationValue.DEV_SCORE
         )
-        return [sentence for sentence in dataset]
 
-
-class Tagger:
-    """Class Tagger: Tags the prepositions in a sentence."""
-
-    def __init__(self):
-        """Tags the prepositions in a sentence.
-
-        **Input**: List with strings
-
-        **Output**: List with strings, marked prepositions
-
-        If a sentence contains several prepositions each preposition will be marked in an individual sentence.
-        """
-        # Load model
-        self.__tagger = SequenceTagger.load('pos')
-        self.__sentences = []
-
-    def set_input(self, inputs):
-        """Set input list with type string of sentences to tag and mark
-           :param inputs: list of strings
-        """
-
-        self.__sentences = inputs
-
-    def do_tagging(self):
-        """Tagging of prepositions. Input has to be set with the *set_input* method.
-           Returns a list of strings with tagged prepositions.
-        """
-
-        predict_sentences = []
-
-        for sentence in self.__sentences:
-            # Predict PoS tags
-            self.__tagger.predict(sentence)
-
-            tagged_sentence = str(sentence.to_tagged_string())
-
-            # Extract all prepositions
-            prepositions = []
-            helper = tagged_sentence
-            while helper.__contains__("<IN>"):
-                temp, helper = helper.split("<IN>", 1)
-                temp = temp.rsplit(">", 1)[1]
-                temp = temp.replace(" ", "")
-                prepositions.append(temp)
-
-            # Substitute preposition with marker
-            tagged_sentence = sub('> [^>]+ <IN>', '> _prep_', tagged_sentence)
-            # Delete all other tags (not needed)
-            tagged_sentence = sub(' <[^>]+>', '', tagged_sentence)
-
-            # Extract position (index) of preposition
-            prep_count = []
-            for match in finditer("_prep_", tagged_sentence):
-                match = str(match)
-                match = ((match.split("span=(")[1]).split("),")[0]).split(",")[0]
-                match = int(match)
-                prep_count.append(match)
-
-            # Insert the prepositions back into the sentence; Every preposition will be inserted once with
-            # <head> & <\head> markers (in an individual sentence)
-            for j in range(len(prep_count)):
-                count = 0  # Counter which preposition (1st, 2nd, 3rd, ...) is inserted next
-                begin = 0  # Counter where to continue inserting the next preposition
-                tmp = ""
-                i = 0
-                for i in prep_count:
-                    # Insert Text from previously inserted preposition to next preposition to insert
-                    if(j == count):
-                        # ... with head-marker
-                        tmp += tagged_sentence[begin:i]+"<head>"+prepositions[count]+"<\head>"
-                    else:
-                        # without head-marker
-                        tmp += tagged_sentence[begin:i] + prepositions[count]
-                    count += 1
-                    begin = i+6
-                # Append rest of sentence
-                tmp += tagged_sentence[i+6:]
-                # Add complete sentence to return list
-                predict_sentences.append(tmp)
-
-        # Returns the list of sentences with marked prepositions
-        return predict_sentences
+        # start the optimization
+        param_selector.optimize(search_space, max_evals=100)
 
 
 def main():
