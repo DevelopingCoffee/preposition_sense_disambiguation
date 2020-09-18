186eb8441fab13b730460ace2bad94eedb1c68f1
Dirk Neuhäuser <dirk.neu3@gmail.com>
2020-09-18 15:09:35 +0200

    doc change

--- huggingface_sense_disambiguation/torch/torch_tagger.py
+++ huggingface_sense_disambiguation/torch/torch_tagger.py
@@ -39,24 +39,29 @@ class BertTagger:
 
 
 tagger = BertTagger()
-tagger.tag("I am <head>in</head> big trouble")
-tagger.tag("I am <head>in</head> a big airplane")
-tagger.tag("I am <head>in</head> New York")
-tagger.tag("<head>In</head> 2020 Donald Trump will be re-elected")
-tagger.tag("I always see you <head>in</head> my dreams.")
-tagger.tag("I am speaking <head>in</head> portuguese.")
-tagger.tag("Donald Trump appears <head>in</head> a weird manner.")
+# IN
+#tagger.tag("I am <head>in</head> big trouble")
+#tagger.tag("I am <head>in</head> a big airplane")
+#tagger.tag("I am <head>in</head> New York")
+#tagger.tag("<head>In</head> 2020 Donald Trump will be re-elected")
+#tagger.tag("I always see you <head>in</head> my dreams.")
+#tagger.tag("I am speaking <head>in</head> portuguese.")
+#tagger.tag("Donald Trump appears <head>in</head> a weird manner.")
 
+# BY
 tagger.tag("<head>By</head> 2021 I will have a bachelor degree.")
 tagger.tag("<head>By</head> the way, Joe Biden is actually better than is Opponent.")
 tagger.tag("The Crisis was handled <head>by</head> the best president ever - Donald Trump.")
 tagger.tag("He is leading the polls <head>by</head> far.")
+
+# WITH
 #tagger.tag("He is swimming <head>with</head> his hands.")
 #tagger.tag("He is <head>with</head> his parents.")
 #tagger.tag("She blinked <head>with</head>  confusion.") # Manner
 #tagger.tag("He combines professionalism <head>with</head>  humor.") # Accompanier
 #tagger.tag("He washed a small red teacup <head>with</head>  water.")  # Means
-#
+
+# ON
 #tagger.tag("The shop is <head>on</head> the left.")
 #tagger.tag("My friend is <head>on</head> the way to Moscow.")
 #tagger.tag("When she was a little girl people saw unrealistic cowboy films <head>on</head> television")
